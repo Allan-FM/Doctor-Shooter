e1aaaac83ba3b5c999da562440ba6fea622aa569
Allan <allanfelipedk@gmail.com>
2022-04-01 21:24:48 -0300

    Character Movement

--- Assets/Runtime/Scripts/Player/PlayerMovement.cs
+++ Assets/Runtime/Scripts/Player/PlayerMovement.cs
@@ -13,13 +13,24 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float minBoundsY = 3.7f;
     [SerializeField] private float maxBoundsY = 0f;
 
+    [Header("Shoot")]
+    [SerializeField] private float shootWaitTime = 0.5f;
+
     private Vector3 tempPos;
     private float xAxis;
     private float yAxis;
+    private PlayerAnimation playerAnimation;
+
+    private void Awake()
+    {
+        playerAnimation = GetComponent<PlayerAnimation>();
+    }
 
     private void Update()
     {
         HandleMovement();
+        HandleAnimation();
+        HandleFacingDirection();
     }
     private void HandleMovement()
     {
@@ -31,7 +42,33 @@ public class PlayerMovement : MonoBehaviour
         tempPos.x += xAxis * speed * Time.deltaTime;
         tempPos.y += yAxis * speed * Time.deltaTime;
 
-        transform.position = tempPos;
+        if(tempPos.y > maxBoundsY)
+        {
+            tempPos.y = maxBoundsY;
+        }
 
+        transform.position = tempPos;
+    }
+    private void HandleAnimation()
+    {
+        if(Mathf.Abs(xAxis) > 0 || Mathf.Abs(yAxis) > 0)
+        {
+            playerAnimation.PlayAnimation(TagManager.WalkAnimationName);
+        }
+        else
+        {
+            playerAnimation.PlayAnimation(TagManager.IdleAnimationName);
+        }
+    }
+    private void HandleFacingDirection()
+    {
+        if(xAxis > 0)
+        {
+            playerAnimation.SetFacingirection(true);
+        }
+        else if(xAxis < 0)
+        {
+            playerAnimation.SetFacingirection(false);
+        }
     }
 }
